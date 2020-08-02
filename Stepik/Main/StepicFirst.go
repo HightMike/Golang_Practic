829f7676ea9849ec1feb7a2b8f38d8b8e55deb7b
hightmike <baobab-borisov@mail.ru>
2020-08-02 23:46:48 +0300

    comment

---
+++ Stepik/Main/StepicFirst.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+//second var for comment
+func main() {
+	var n float64
+	fmt.Scan(&n)
+
+	if n <= 0 {
+		fmt.Printf("число %2.2f не подходит", n)
+	} else if n > 10000 {
+		fmt.Printf("%e", n)
+	} else {
+		fmt.Printf("%.4f", n*n)
+	}
+
+}
